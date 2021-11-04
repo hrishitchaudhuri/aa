c60d31cd56391e4f2aa1b09deb40606046ad0ea9
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2021-11-04 11:31:38 +0530

    Fixed merge conflict in gitignore

--- image.py
+++ image.py
@@ -8,10 +8,28 @@ import matrix
 atj = cv2.imread('atj_grayscale.jpg', 0)
 
 print(atj.shape)
+x, y = atj.shape
 
 m1 = np.fft.fft2(atj)
 m1 = np.fft.ifft2(m1)
 
+m2 = []
+for i in range(x):
+    m2.append([])
+    for j in range(y):
+        m2[i].append(round(m1[i][j].real))
+
+m2 = np.array(m2)
+
+cv2.imwrite('atj_fft_2.jpg', m2)
+"""
+with open('atj_fft.txt', 'w') as atj:
+    for i in range(x):
+        for j in range(y):
+            atj.write(str(int(m2[i][j])) + " ")
+        atj.write("\n")
+"""
+"""
 m2 = matrix.matrix_compression(atj)
 
 print(np.allclose(m1, m2, rtol=1e-3, atol=1e-3))
@@ -29,3 +47,4 @@ with open('hc.txt', 'w') as h:
         for j in range(y):
             h.write(str(m2[i][j]))
         h.write("\n")
+"""
