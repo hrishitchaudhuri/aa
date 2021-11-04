69fe99540d97e4acf8dcc5001c45db24e0ec6e74
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-04 10:51:34 +0530

    Prime number generation

--- Keys.py
+++ Keys.py
@@ -1,27 +1,47 @@
-import rsa
+from random import choice
 
-def keygen():
+def fermat_base2_test(n):
     '''
-        Generates Public and Private keys using the rsa module and
-        writes them into key files
+        Runs base2 primality test on n with k trials,
+        returns 0 for composite, 1 for prime
     '''
-    pkey, pvkey=rsa.newkeys(1024)
-    with open('./keys/pub.pem','wb') as pk:
-        pk.write(pkey.save_pkcs1('PEM'))
-    with open('./keys/pvk.pem','wb') as pvk:
-        pvk.write(pvkey.save_pkcs1('PEM'))
-    pk.close()
-    pvk.close()
+    if pow(2,n-1,n)==1:
+        return 1
+    return 0
 
-def load_keys():
+def generate_large_odd(bitlength):
     '''
-        loads keys from pvk.pem and pub.pem files into variables and
-        return them
+        Randomly generate a binary string of given bit length (ending with 1 to make sure its odd)
+        input: bit length
+        output: odd number of given bit length
     '''
-    with open('./keys/pub.pem','r') as f:
-        pvkey=rsa.PublicKey.load_pkcs1(f.read())
+    num1='0b1'
+    for i in range(bitlength-2):
+        num1 += choice(['0','1'])
+    num1 += '1'
+    num1=(int(num1,2))
+    return num1
+
+
+def generate_large_primes(bitlength):
+    '''
+        generates 2 large odd numbers of given bit length,
+        runs fermat and M-R test and trial division for primality
+        return 2 large primes of given bitlength
+
+    '''
+    num1=generate_large_odd(bitlength)
+    num2=generate_large_odd(bitlength)
+
+    # RUN TESTS FOR PRIMALITY
+    # 1. Fermat's pseudo primality test
+    while fermat_base2_test(num1)==0:
+        num1=generate_large_odd(bitlength)
+    while fermat_base2_test(num2)==0:
+        num2=generate_large_odd(bitlength)
     
-    with open('./keys/pvk.pem','r') as f:
-        pkey=rsa.PrivateKey.load_pkcs1(f.read())
+    # 2. Miller-Rabin primality test
     
-    return pkey, pvkey
+
+    return num1,num2
+    
