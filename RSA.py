be4bf2124ecc3e65aa89ab6938ebc6df86d063c2
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-17 17:25:25 +0530

    sigh

--- RSA.py
+++ RSA.py
@@ -1,59 +1,62 @@
-from random import *
-import numpy as np
+'''RSA IMPLEMENTATION'''
+from random import randint, choice
 import math
 
-def fermat_base2_test(n, k):
+
+def fermat_base2_test(num, trials):
     '''
     Runs base2 primality test on n with k trials,
     returns 0 for composite, 1 for prime
     '''
-    for i in range(k):
-        a = randint(2, n-2)
-        if pow(a,n-1,n) == 1:
-            return 1
-    return 0
+    for trial in range(trials):
+        a = randint(2, num-2)
+        if pow(a, num-1, num) != 1:
+            return 0
+    return 1
 
-def trial_division(n, trials):
+
+def trial_division(num, trials):
     '''
     Trial Division Test
     '''
-    for i in range(trials):
-        check = randint(2,int(math.sqrt(n)))
-        if n%check == 0:
+    for trial in range(trials):
+        check = randint(2, int(math.sqrt(num)))
+        if num % check == 0:
             return False
     return True
 
 
-def miiller_test_helper(d, n):
-    a = 2 + randint(1, n - 4);
-    x = pow(a, d, n);
-    if (x == 1 or x == n - 1):
-        return True;
-    while (d != n - 1):
-        x = (x * x) % n;
-        d *= 2;
-        if (x == 1):
-            return False;
-        if (x == n - 1):
-            return True;
-    return False;
+def miiller_test_helper(d, num):
+    a = 2 + randint(1, num - 4)
+    temp = pow(a, d, num)
+    if temp == 1 or temp == num - 1:
+        return True
+    while d != num - 1:
+        temp = (temp * temp) % num
+        d *= 2
+        if temp == 1:
+            return False
+        if temp == num - 1:
+            return True
+    return False
 
-def miller_test( n, k):
+
+def miller_test(num, trials):
     '''
     Miller Rabin primality test
     '''
-    if (n <= 1 or n == 4):
-        return False;
-    if (n <= 3):
-        return True;
-    d = n - 1;
-    while (d % 2 == 0):
-        d //= 2;
-    for i in range(k):
-        if (miiller_test_helper(d, n) == False):
-            return False;
-    return True;
-    
+    if num <= 1 or num == 4:
+        return False
+    if num <= 3:
+        return True
+    d = num - 1
+    while d % 2 == 0:
+        d //= 2
+    for trial in range(trials):
+        if miiller_test_helper(d, num) == False:
+            return False
+    return True
+
 
 def generate_large_odd(bitlength):
     '''
@@ -61,12 +64,12 @@ def generate_large_odd(bitlength):
         input: bit length
         output: odd number of given bit length
     '''
-    n = '0b1'
-    for i in range(bitlength-2):
-        n += choice(['0','1'])
-    n += '1'
-    n=(int(n,2))
-    return n
+    num = '0b1'
+    for i in range(bitlength - 2):
+        num += choice(['0', '1'])
+    num += '1'
+    num = (int(num, 2))
+    return num
 
 
 def generate_large_primes(bitlength):
@@ -75,62 +78,64 @@ def generate_large_primes(bitlength):
         runs fermat and M-R test and trial division for primality
         return 2 large primes of given bitlength
     '''
-    num1=generate_large_odd(bitlength)
-    num2=generate_large_odd(bitlength)
-    
-    while not(fermat_base2_test(num1,3) and miller_test(num1,4) and trial_division(num1,100)):
-        num1=generate_large_odd(bitlength)
-    while not(fermat_base2_test(num2,3) and miller_test(num2, 4) and trial_division(num2,100)):
-        num2=generate_large_odd(bitlength)
+    num1 = generate_large_odd(bitlength)
+    num2 = generate_large_odd(bitlength)
+    while not(fermat_base2_test(num1, 3) and miller_test(num1, 4) and trial_division(num1, 100)):
+        num1 = generate_large_odd(bitlength)
+    while not(fermat_base2_test(num2, 3) and miller_test(num2, 4) and trial_division(num2, 100)):
+        num2 = generate_large_odd(bitlength)
+    return num1, num2
 
-    return num1,num2
 
-def gcd(a, b):
-   while a != 0:
-      a, b = b % a, a
-   return b
+def gcd(first, second):
+    while first != 0:
+        first, second = second % first, first
+    return second
+
 
 def find_mod_inverse(a, m):
-   if gcd(a, m) != 1:
-      return None
-   u1, u2, u3 = 1, 0, a
-   v1, v2, v3 = 0, 1, m
-   
-   while v3 != 0:
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
         q = u3 // v3
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
-   return u1 % m
+    return u1 % m
+
 
 def keygen(size):
     '''
     Generates RSA public and private keys of given bitlength
     '''
-    p,q = generate_large_primes(size)
+    p, q = generate_large_primes(size)
     n = p*q
     e = choice([2*i+1 for i in range(100)])
 
     while gcd(e, (p-1)*(q-1)) != 1:
         e = choice([2*i+1 for i in range(100)])
-    
-    d = find_mod_inverse(e,(p-1)*(q-1))
-    pubkey = (e,n)
-    privkey = (d,n)
-    return {'public':pubkey,'private':privkey}
+    d = find_mod_inverse(e, (p - 1) * (q - 1))
 
-def __encrypt(block, e, n, keysize=128):
+    pubkey = (e, n)
+    privkey = (d, n)
+    return {'public':pubkey, 'private':privkey}
+
+
+def __encrypt(block, enc, N, keysize=128):
     '''
         keysize byte block encrypted/decrypted using pubkey/privkey
     '''
-    c = b''
+    cipher_block = b''
     blocknum = int.from_bytes(block, 'big')
-    result = pow(blocknum, e, n).to_bytes(keysize,'big')
+    result = pow(blocknum, enc, N).to_bytes(keysize, 'big')
+
     for i in result:
         if i != 0:
-            c += i.to_bytes(1,'big')
-    return c
+            cipher_block += i.to_bytes(1, 'big')
+    return cipher_block
 
 
-def encrypt(msg, e, n, keysize=128):
+def encrypt(msg, enc, N, keysize=128):
     '''
         Only pass bytes/bytearray as msg input
 
@@ -143,19 +148,19 @@ def encrypt(msg, e, n, keysize=128):
         same function is called for decrypt as well (pass e = d for decryption)
     '''
     block_size = int((keysize/32)*8)
-    pad_size = block_size - len(msg)%block_size
+    pad_size = block_size - len(msg) % block_size
 
     if pad_size != 0:
-        msg += b'\x00'*pad_size
+        msg += b'\x00' * pad_size
+
     if len(msg) <= block_size:
-        return __encrypt(msg, e, n, keysize=keysize)
+        return __encrypt(msg, enc, N, keysize=keysize)
 
     cipher = b''
     i = 0
-    while i <= len(msg)-block_size:
-        block = msg[i:i+block_size]
-        cipher = cipher + __encrypt(block,e,n)
+    while i <= len(msg) - block_size:
+        block = msg[i: i + block_size]
+        cipher = cipher + __encrypt(block, enc, N)
         i += block_size
 
     return cipher
-
--- rsa_tests.py
+++ rsa_tests.py
@@ -1,76 +1,78 @@
+'''tests for rsa.py'''
 from RSA import encrypt, keygen
 import rsa
 
 def keytest():
-    k = [32, 64, 128, 256, 512, 1024, 2048]
+    '''
+    tests success rate for varying keylengths
+    '''
+    bitlens = [128, 256, 512]
     trials = 100
+    msg = '01234'
+    msg2 = '!@#$%'
     try:
-        for i in k:
-            print(f'------Testing for bitlength {i}------')
-            passed1=0
-            passed2=0
-            msg='01234'
-            msg2='!@#$%'
+        for bitlen in bitlens:
+            print(f'------Testing for bitlength {bitlen}------')
+            passed1 = 0
+            passed2 = 0
             # msg='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=~`'
             # msg2='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-            for j in range(trials):
-                keys=keygen(i)
-
-                c1=encrypt(msg.encode('utf-8'),keys['public'][0],keys['public'][1], keysize=i)
-                d1=encrypt(c1,keys['private'][0], keys['private'][1], keysize=i)
-
-                c2=encrypt(msg2.encode('utf-8'),keys['public'][0],keys['public'][1], keysize=i)
-                d2=encrypt(c2,keys['private'][0], keys['private'][1], keysize=i)
-
-                if d1==msg.encode('utf-8'):
+            for trial in range(trials):
+                keys = keygen(bitlen)
+                c1 = encrypt(msg.encode('utf-8'), keys['public'][0], keys['public'][1], keysize=bitlen)
+                d1 = encrypt(c1, keys['private'][0], keys['private'][1], keysize=bitlen)
+                c2 = encrypt(msg2.encode('utf-8'), keys['public'][0], keys['public'][1], keysize=bitlen)
+                d2 = encrypt(c2, keys['private'][0], keys['private'][1], keysize=bitlen)
+                if d1 == msg.encode('utf-8'):
                     passed1 += 1
-                if d2==msg2.encode('utf-8'):
+                if d2 == msg2.encode('utf-8'):
                     passed2 += 1
+            print(f'''{bitlen} bits enc/dec\nmsg1: {(passed1/trials)*100}% of the time\nmsg2: {(passed2/trials)*100}% of the time''')
 
-            print(f'{i} bits enc/dec passed \nmsg1: {(passed1/trials)*100}% of the time\nmsg2: {(passed2/trials)*100}% of the time')
-                
-            
     except OverflowError:
-        print(f'overflowed for bitlength {i} at iteration {j}')
-        pass
+        print(f'overflowed for bitlength {bitlen} at iteration {trial}')
+
 
 def module_test():
-    k = [32, 64, 128, 256, 512, 1024, 2048]
+    '''
+    compares with python-rsa module
+    '''
+    bit_lengths = [128, 256, 512]
     trials = 100
     # msg1='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     # msg2='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=~`'
     msg1 = '01235'
     msg2 = '!@#$*'
-    for i in k:
-        p1, p2 = 0, 0
-        rp1, rp2 = 0, 0
-        print(f'------Testing for {i} bits------')
-        for j in range(trials):
-            pub, priv = rsa.newkeys(nbits=i)
-            keys = keygen(i)
+    try:
+        for i in bit_lengths:
+            p1, p2 = 0, 0
+            rp1, rp2 = 0, 0
+            print(f'------Testing for {i} bits------')
+            for trial in range(trials):
+                pub, priv = rsa.newkeys(nbits=i)
+                keys = keygen(i)
+                rc1 = rsa.encrypt(msg1.encode('utf-8'), pub)
+                rd1 = rsa.decrypt(rc1, priv)
+                c1 = encrypt(msg1.encode('utf-8'), keys['public'][0], keys['public'][1], keysize=i)
+                d1 = encrypt(c1, keys['private'][0], keys['private'][1], keysize=i)
+                rc2 = rsa.encrypt(msg2.encode('utf-8'), pub)
+                rd2 = rsa.decrypt(rc2, priv)
+                c2 = encrypt(msg2.encode('utf-8'), keys['public'][0], keys['public'][1], keysize=i)
+                d2 = encrypt(c2, keys['private'][0], keys['private'][1], keysize=i)
+                if d1 == msg1.encode('utf-8'):
+                    p1 += 1
+                if d2 == msg2.encode('utf-8'):
+                    p2 += 1
+                if rd1 == msg1.encode('utf-8'):
+                    rp1 += 1
+                if rd2 == msg2.encode('utf-8'):
+                    rp2 += 1
+            print(f'rsa:\nmsg1: passed {rp1}%\nmsg2: passed {rp2}%')
+            print(f'imp:\nmsg1: passed {p1}%\nmsg2: passed {p2}%')
 
-            rc1 = rsa.encrypt(msg1.encode('utf-8'),pub)
-            rd1 = rsa.decrypt(rc1, priv)
-            c1=encrypt(msg1.encode('utf-8'),keys['public'][0],keys['public'][1], keysize=i)
-            d1=encrypt(c1,keys['private'][0], keys['private'][1], keysize=i)
-            
-            rc2 = rsa.encrypt(msg2.encode('utf-8'),pub)
-            rd2 = rsa.decrypt(rc2,priv)
-            c2=encrypt(msg2.encode('utf-8'),keys['public'][0],keys['public'][1], keysize=i)
-            d2=encrypt(c2,keys['private'][0], keys['private'][1], keysize=i)
+    except OverflowError:
+        print(f'overflowed for {i} bits at iteration {trial}')
 
-            if d1==msg1.encode('utf-8'):
-                p1+=1
-            if d2==msg2.encode('utf-8'):
-                p2+=1
-            if rd1==msg1.encode('utf-8'):
-                rp1+=1
-            if rd2==msg2.encode('utf-8'):
-                rp2+=1
-        print(f'rsa:\nmsg1: passed {rp1}%\nmsg2: passed {rp2}%')
-        print(f'imp:\nmsg1: passed {p1}%\nmsg2: passed {p2}%')
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     keytest()
-    #module_test()
+    module_test()
